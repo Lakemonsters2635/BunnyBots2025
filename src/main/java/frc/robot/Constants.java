16f3737e107761377c0618ec441a393c81020608
Lakemonsters2635 <lakemonsters.software.team@gmail.com>
2025-10-13 20:51:07 -0700

    intial commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,13 @@
+package frc.robot;
+
+public class Constants {
+    public static final double kPModuleTurningController = 0.5;
+    public static final double kPModuleDriveController = 0;
+    
+    public static final double kMaxSpeedMetersPerSecond = 4.0;
+    // TODO: 'EncoderDistancePerPulse' should be calculated based on the gearing and wheel diameter
+    public static final double kDriveEncoderDistancePerPulse = 0.0011265396; 
+    
+    public static final String CAN_BUS_NAME = "CANivore";
+    
+}
