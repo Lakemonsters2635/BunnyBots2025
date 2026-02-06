741638f22fb8323a42bc646fffe78e2bbf78557c
SahanRULES <sahanvuyyuru@gmail.com>
2026-02-05 19:24:17 -0800

    commit with cubic smoothing function on drive

--- src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -526,9 +526,9 @@ public class DrivetrainSubsystem extends SubsystemBase {
       //         true);
       if(followJoystics){
         this.drive(
-          xPowerCommanded * DrivetrainSubsystem.kMaxSpeed,
-          yPowerCommanded * DrivetrainSubsystem.kMaxSpeed,
-          MathUtil.applyDeadband(rotCommanded * this.kMaxAngularSpeed, 0.2),
+          Math.pow(xPowerCommanded,3) * DrivetrainSubsystem.kMaxSpeed,
+          Math.pow(yPowerCommanded,3) * DrivetrainSubsystem.kMaxSpeed,
+          MathUtil.applyDeadband(Math.pow(rotCommanded,3) * this.kMaxAngularSpeed, 0.2),
           true);
       }
       
